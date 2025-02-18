6a7f22ff4e3de9c2c3100a3787df48c428284238
RiccardoS <rspinellir13@gmail.com>
2025-02-18 17:04:43 +0100

    First push

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+Remove node_modules from Git History
+I need to remove packaje json from Git tracking:
+
+# Remove node_modules from tracking
+git rm -r --cached node_modules
+
+# Commit the change
+git commit -m "Removed node_modules from repository"
+
+# Push the changes to GitHub
+git push origin main */
